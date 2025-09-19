0f1f960fcc5638f4a7ce678cc5d76e61070b28bd
Bela <imcdsls10@gmail.com>
2025-09-19 01:01:20 -0300

    Handlebars alterar adicionado

--- controllers/PostagensController.js
+++ controllers/PostagensController.js
@@ -1,1 +1,59 @@
-con
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post.js');
+const User = require('../models/User.js');
+
+//Rota principal
+router.get('/blog', function(req, res) {
+    res.render('home');
+});
+
+//Rotas do User
+
+// Rota para o formulário de cadastro
+router.get('/cadastro', function (req, res) {
+    res.render('cadastro');
+});
+
+//Rotas do Post
+
+//Rota para adicionar post
+router.post('/add', function (req, res) {
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo,
+        fk_user: req.body.fk_user
+    }).then(() => res.redirect('/postagens'))
+    .catch(erro => res.send('Houve um erro: ' + erro));
+});
+
+//Rota para deletar um post
+router.get('/deletar/:id', function (req, res) {
+    Post.destroy({ where: { id: req.params.id } })
+    .then(() => res.redirect('/postagens'))
+    .catch(() => res.send("Essa postagem não existe!"));
+});
+
+// Rota para exibir o formulário de alteração
+router.get('/alterar/:id', function (req, res) {
+    Post.findAll({ where: { id: req.params.id } }).then(posts => {
+        posts = posts.map(post => post.toJSON());
+        res.render('alterar', {posts: posts});
+    });
+});
+
+//Rota para atualizar post
+router.post('/update', function (req, res) {
+    Post.update(
+        {
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        },
+        {
+            where: { id: req.body.id }
+        }
+    ).then(() => res.redirect('/postagens'))
+    .catch(erro => res.send("Erro ao atualizar postagem: " + erro));
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //Servidor 
-
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const rota_post = require('./controllers/PostagensController');
+const rota_post = require('./controllers/PostagensController.js');
 
 //Configurando o handlebars
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
@@ -15,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //Usando rotas 
-app.use('/home', rota_post);
+app.use('/blog', rota_post);
 
 app.listen(8081, function() {
     console.log("Servidor rodando na porta 8081!")
