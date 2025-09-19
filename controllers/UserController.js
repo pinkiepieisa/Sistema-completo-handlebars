25b45886fc2d29b52cbea83ffaa2026c5570a270
Bela <imcdsls10@gmail.com>
2025-09-19 14:18:45 -0300

    Alteração da rota inicial

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,17 +14,6 @@ router.get('/cad', function (req, res) {
     res.render('cadastro');
 });
 
-// Rota para o formulário de cadastro
-router.post('/cad', function (req, res) {
-    User.create({
-        nome: req.body.nome,
-        email: req.body.email,
-        usuario: req.body.usuario,
-        senha: req.body.senha
-    }).then(() => res.redirect('/home'))
-    .catch(erro => res.send('Houve um erro: ' + erro));
-});
-
 //Rota para o login
 router.get('/login', function(req, res) {
     res.render('login');
@@ -35,17 +24,15 @@ router.get('/home', function(req, res) {
     res.render('home');
 });
 
-router.post('/login', function(req, res) {
-    User.findAll(
-        { 
-            where: { email: req.params.email }
-        } 
-        &&
-        { 
-            where: { senha: req.params.senha }
-        })
-    .then(() => redirect('/home/postagens'))
-    .catch(() => res.send("Esse usuário não existe!"));
+// Rota para o formulário de cadastro
+router.post('/cad', function (req, res) {
+    User.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        usuario: req.body.usuario,
+        senha: req.body.senha
+    }).then(() => res.redirect('/home'))
+    .catch(erro => res.send('Houve um erro: ' + erro));
 });
 
 module.exports = router;
